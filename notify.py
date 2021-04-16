a79c6ce2b07d868a32ba44b537e2e31c93af4b95
allenzechini <allen@shotover.com>
2021-04-16 10:53:36 -0600

    Added email details

--- notify.py
+++ notify.py
@@ -1,11 +1,19 @@
 import smtplib, ssl
 
 port = 465  # For SSL
-password = input("Type your password and press enter: ")
+password = ""
+
+sender_email = "alerts@shotover.com"
+receiver_email = "allen@shotover.com, logan@shotover.com"
+message = """\
+    Subject: Hi there
+
+    This message is sent from Python.
+    """
 
 # Create a secure SSL context
 context = ssl.create_default_context()
 
 with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
-    server.login("my@gmail.com", password)
-    # TODO: Send email here
+    server.login("alerts@shotover.com", password)
+    server.sendmail(sender_email, receiver_email, message)
