fb9f031257e2ed9dd824f0208347ecc96f3c451a
Allen Zechini <allen@shotover.com>
2021-04-26 17:05:27 +0000

    Adding HTML diff

---
+++ create-html-diff.py
@@ -0,0 +1,34 @@
+import argparse
+import difflib
+
+def do_diff(file1='', file2=''):
+  f1 = open(file1)
+  f2 = open(file2)
+  lines1 = f1.readlines()
+  lines2 = f2.readlines()
+  f2.close()
+  f1.close()
+
+  diff = difflib.HtmlDiff().make_file(lines1, lines2, 
+                                      file1, file2,
+                                      context=True, numlines=10)
+
+  my_diff = "/tmp/named.conf.block.htmldiff"
+  with open(my_diff, 'w') as output:
+    output.write(diff)
+  output.close()
+
+def main():
+  parser = argparse.ArgumentParser()
+  parser.add_argument('-f1', '--file1', help='First file to diff')
+  parser.add_argument('-f2', '--file2', help='Second file to diff')
+  args = parser.parse_args()
+
+  if args.file1 == None or args.file2 == None:
+    print('\nPlease pass two files to compare')
+    return 1
+  do_diff(args.file1, args.file2)
+
+if __name__ == '__main__':
+  main()
+
--- update-dns-blacklist.sh
+++ update-dns-blacklist.sh
@@ -16,6 +16,20 @@ restore_blacklist() {
 	cp ${WORKING_BACKUP} ${WORKING}
 }
 
+cleanup() {
+	rm ${NEW_BLACKLIST} ${HTMLDIFF}
+	# rm ${HTMLDIFF_ZIP}
+}
+
+create_diff() {
+  python3 ${HTML_DIFF} -f1 ${WORKING} -f2 ${NEW_BLACKLIST}
+}
+
+notify() {
+  python3 ${NOTIFY}
+  # ansible-playbook notify.yml
+}
+
 run_scraper() {
 	python3 ${SCRAPER} > /dev/null 2>&1
 
@@ -26,33 +40,29 @@ run_scraper() {
     exit 0
   else
     backup_blacklist
+    create_diff
 	  cp ${NEW_BLACKLIST} ${WORKING}
   fi
-}
-
-cleanup() {
-	rm ${NEW_BLACKLIST}
-}
-
-notify() {
-  python3 ${NOTIFY}
 }
 
 # bind vars
 BIND_HOME=/etc/bind
 WORKING=${BIND_HOME}/named.conf.block
 WORKING_BACKUP=${BIND_HOME}/named.conf.block.working_backup
-NEW_BLACKLIST=${BIND_HOME}/named.conf.block.new    # hardcoded in scraper
+NEW_BLACKLIST=${BIND_HOME}/named.conf.block.new
 
 # Project vars
-PROJECT_HOME=/home/allen.zechini/projects/update-dns-blacklist
+PROJECT_HOME=/home/azechini/projects/update-dns-blacklist
+HTML_DIFF=${PROJECT_HOME}/create-html-diff.py
 SCRAPER=${PROJECT_HOME}/scraping-bad-dns.py
 NOTIFY=${PROJECT_HOME}/notify.py
 
 # Other vars
 TIMESTAMP=$(date +%Y%m%d_%H%M%S)
 LOGFILE=/var/log/update-dns-blacklist.log
-USERENV=/home/allen.zechini/.bash_profile
+USERENV=/home/azechini/.bash_profile
+HTMLDIFF=/tmp/named.conf.block.htmldiff
+# HTMLDIFF_ZIP=/tmp/named.conf.block.htmldiff.zip
 
 ### MAIN ###
 
