15ad4cdd9d4b0bf33639b37ff200e804a1ea33d3
Allen Zechini <allen@shotover.com>
2021-05-27 17:15:10 +0000

    Remove unnecessary functions

--- update-dns-blacklist.sh
+++ update-dns-blacklist.sh
@@ -8,33 +8,16 @@
 #                                                   #
 #####################################################
 
-backup_blacklist() {
-	cp ${WORKING} ${WORKING_BACKUP}
-}
-
-restore_blacklist() {
-	cp ${WORKING_BACKUP} ${WORKING}
-}
-
-cleanup() {
-	rm ${NEW_BLACKLIST}
-}
-
-notify() {
-  python3 ${NOTIFY}
-  # ansible-playbook notify.yml
-}
-
 run_scraper() {
 	python3 ${SCRAPER} > /dev/null 2>&1
 
   diff ${WORKING} ${NEW_BLACKLIST}
   if [ $? = 0 ]; then
     echo "${TIMESTAMP}: No blacklist changes...nothing to do" >> ${LOGFILE}
-    cleanup
+    rm ${NEW_BLACKLIST}
     exit 0
   else
-    backup_blacklist
+    cp ${WORKING} ${WORKING_BACKUP}
 	  cp ${NEW_BLACKLIST} ${WORKING}
   fi
 }
@@ -63,14 +46,14 @@ run_scraper
 sudo systemctl restart bind9
 if [ $? != 0 ]; then
   echo "${TIMESTAMP}: New blacklist caused bind9 to fail...reverting blacklist" >> ${LOGFILE}
-	restore_blacklist
+  cp ${WORKING_BACKUP} ${WORKING}
 	systemctl restart bind9
 else
   . ${USERENV} > /dev/null 2>&1
-  notify
+  python3 ${NOTIFY}
   NUM=$(diff -y --suppress-common-lines ${WORKING} ${WORKING_BACKUP} | wc -l)
   echo "${TIMESTAMP}: Blacklist updated successfully (${NUM} changes)" >> ${LOGFILE}
 fi
 
-cleanup
+rm ${NEW_BLACKLIST}
 
